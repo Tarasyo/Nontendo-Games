9d32cc57a112115250fe39f2a3e5f4cca5ceddee
Tarasyo <tarasyo7@gmail.com>
2020-04-26 10:26:40 +0000

    adding navigation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
 
-function App() {
+
+
+import './App.css';
+import Users from './types/pages/Users.js';
+import NewPlace from './game/games/NewGame.js/index.js';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
+import TypeGames from './game/games/TypeGames';
+
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Router>
+    <MainNavigation />
+    <main>
+    <Switch>
+      <Route path="/" exact>
+        <Users />
+      </Route>
+      <Route path="/:userId/places" exact>
+        <TypeGames />
+      </Route>
+      <Route path="/places/new" exact>
+        <NewPlace />
+      </Route>
+      <Redirect to="/" />
+      </Switch>
+      </main>
+  </Router>
   );
-}
+};
 
 export default App;
